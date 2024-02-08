e7041a3a0269cc0ec190f1a0f9b78459a06c0be5
serwaapreimann <86732266+serwaapreimann@users.noreply.github.com>
2024-02-07 23:28:54 -0800

    Warehouse List showing

--- Instock-fe/src/App.js
+++ Instock-fe/src/App.js
@@ -4,19 +4,41 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 // import components
 import Header from "./components/header/Header";
 import Footer from "./components/Footer/Footer";
-// import Warehouse from "./components/pages/warehouse/Warehouse";
-// import WarehouseDetails from "./components/warehouseDetails/WarehouseDetails";
+import Warehouse from "./components/Warehouse/Warehouse";
+import WarehouseDetails from "./components/Warehouse Details/WarehouseDetails";
+import { useState, useEffect } from "react";
+import Axios from "axios"
 // import EditWarehouse from "./components/editwarehouse/EditWarehouse";
 // import AddWarehouse from "./components/addwarehouse/AddWarehouse";
 // import Inventory from "./components/pages/inventory/Inventory";
 // import InventoryDetails from "./components/inventoryDetails/InventoryDetails";
 // import DeleteWarehouse from "./components/deleteWarehouse/DeleteWarehouse";
 // import EditInventory from "./components/editInventory/EditInventory";
-import AddInventory from "./components/AddInventory/AddInventory";
+// import AddInventory from "./components/AddInventory/AddInventory";
 // import DeleteInventory from "./components/delteInventory/DeleteInventory";
 
 function App() {
 
+  useEffect(() => {
+    document.title = 'InStock | Where Inventory Meets Excellence. '; 
+  }, []);
+    const [warehouses, setWarehouses] = useState([]);
+
+
+    useEffect(() =>{
+      const fetchWarehouse = async () => {
+        try {
+          const urlAPI = "http://localhost:8088/api/warehouses";
+          const response = await Axios.get(urlAPI); 
+          setWarehouses(response.data.warehouseListed);
+          console.log(response.data.warehouseListed);
+        } catch (error) {
+          console.error("Error fetching warehouse:", error.message);
+        }
+      };
+  
+      fetchWarehouse(); // Moved outside the try-catch block
+    }, []); 
 
 
   return (
@@ -26,18 +48,18 @@ function App() {
           <Header />
           <Routes>
             {/* Warehouse routes */}
-            {/* <Route path="/" exact element={<Warehouse />} />
-            <Route path="/warehouse" element={<Warehouse />} />
+             <Route path="/" exact element={<Warehouse warehouses ={warehouses}  />} />
+            <Route path="/warehouse" element={<Warehouse warehouses={warehouses} />}  />
             <Route path="/warehouse/:id" element={<WarehouseDetails />} />
-            <Route path="/warehouse/add" element={<AddWarehouse />} />
-            <Route path="/warehouse/:id/edit" element={<EditWarehouse />} /> */}
+            {/* <Route path="/warehouse/add" element={<AddWarehouse />} />
+            <Route path="/warehouse/:id/edit" element={<EditWarehouse />} />  */}
 
             {/* Inventory routes */}
             {/* <Route path="/inventory" element={<Inventory />} />
             <Route path="/inventory/:id" element={<InventoryDetails />} />
             <Route path="/inventory/:id/edit" element={<EditInventory />} /> */}
 
-            <Route path="/inventory/add" element={<AddInventory/>} />
+            {/* <Route path="/inventory/add" element={<AddInventory/>} /> */}
             {/* <Route path="/inventory/:id/delete" element={<DeleteInventory />} /> */}
 
           </Routes>
